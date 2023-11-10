746cf164452123ec9beb976d7bcd74fc74268a4e
shalom-nganga <minnomacharia@gmail.com>
2023-11-11 00:00:59 +0300

    completed assignments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+// import {name, city} from "../data/data.js";
 
+
+const h1Style = {
+  color: 'firebrick',
+  fontSize: '40px',
+}
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={h1Style}>
+        Minno is a Web Developer from Kenya
+      </h1>
+    </div>
+ 
+
+  );
+
 }
 
 export default Home;
